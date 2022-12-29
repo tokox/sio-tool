12f9a8ce77a0f6c3884927866800ee62cb9552b5
Arapak <kajetan.ramsza@protonmail.com>
2022-12-29 17:05:42 +0100

    fix upgrade

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -77,7 +77,7 @@ func getLatest() (version, note, ptime, url string, size uint, err error) {
 	note = result["body"].(string)
 	tm, _ := time.Parse("2006-01-02T15:04:05Z", result["published_at"].(string))
 	ptime = tm.In(time.Local).Format("2006-01-02 15:04")
-	url = fmt.Sprintf("https://github.com/Arapak/sio-tool/releases/download/%v/st_%v_%v_%v.zip", version, version, goos, arch)
+	url = fmt.Sprintf("https://github.com/Arapak/sio-tool/releases/download/%v/st_%v_%v.zip", version, goos, arch)
 	assets, _ := result["assets"].([]interface{})
 	for _, tmp := range assets {
 		asset, _ := tmp.(map[string]interface{})
