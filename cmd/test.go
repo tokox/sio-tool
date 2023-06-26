44ce96fe6e726801dbd3e7e9f98afd1d3c247751
Arapak <kajetan.ramsza@protonmail.com>
2023-06-26 16:27:04 +0200

    better test verdicts

--- cmd/test.go
+++ cmd/test.go
@@ -132,7 +132,9 @@ func generateVerdict(testID, answer string, processInfo ProcessInfo) Verdict {
 	} else {
 		correct = false
 		state = color.New(color.FgRed).Sprintf("Failed #%v", testID)
+		diff += color.New(color.FgCyan).Sprintf("-----Output-----\n")
 		diff += output + "\n"
+		diff += color.New(color.FgCyan).Sprintf("-----Answer-----\n")
 		diff += answer + "\n"
 	}
 	return Verdict{correct, fmt.Sprintf("%v ... %.3fs %v\n%v", state, processInfo.time, processInfo.memory, diff), nil}
